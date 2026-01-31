99863bf9b12893e997a3490bc4e096e9d87a1652
vasanthvconsultant-cloud <vasanthvconsultant@gmail.com>
2026-01-31 17:16:17 +0530

    Fix typo in home route response message

--- app.py.py
+++ app.py.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "It'Me Vasanth I Create Flask app completed!"
+    return "It'Me Vasanth I Have Create Flask app completed!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
 
 
