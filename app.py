e807f9775550f4de7e67d7a25ac6ad23598c4c92
vasanthvconsultant-cloud <vasanthvconsultant@gmail.com>
2026-01-31 16:14:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def calculator():
             result = "Error: " + str(e)
     return render_template_string(html, result=result)
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+app.run(host='0.0.0.0', port=5000, debug=True)
+
